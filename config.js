2a2bd11497c32740082bb0ebfe7f9ae72945afe2
Push-d <119042846+Push-d@users.noreply.github.com>
2023-04-10 16:06:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,32 +1,19 @@
 /* ---------------------------------------------------------------------------------/
 /                                                                                   /
 /             d8888 888    888                        888b     d888 8888888b.       /
-/            d88888 888    888                        8888b   d8888 888  "Y88b      /
-/           d88P888 888    888                        88888b.d88888 888    888      /
-/          d88P 888 888888 888  8888b.  .d8888b       888Y88888P888 888    888      /
-/         d88P  888 888    888     "88b 88K           888 Y888P 888 888    888      /
-/        d88P   888 888    888 .d888888 "Y8888b.      888  Y8P  888 888    888      /
-/       d8888888888 Y88b.  888 888  888      X88      888   "   888 888  .d88P      /
-/      d88P     888  "Y888 888 "Y888888  88888P'      888       888 8888888P"       /
+/                                                                                   /
+/           88 888    888                        8888888 888    888                 /
+/          888 888888 888  8.  .d8888b       888Y88888P888 888    888               /
+/         d888     "88b 88K           888 888    888                                /
+/        d88P                                                                       /
+/        Y88b.  888 888  888      X88      888   "   888 888  .d88P                 /
+/      d88P     888  "Y888  88888P'      888       888 8888888P"                    /
 /                                                                                   / 
 /-----------------------------------------------------------------------------------/
-/ Author and Main Developer: FantoX                                                 /
-/ Github: https://github.com/FantoX001/Atlas-MD                                     /
-/ Powered By: Team ATLAS                                                            /
-/-----------------------------------------------------------------------------------/
-/             Meet Team ATLAS who holds all rights to this repository:              /
-/                                                                                   /
-/ 1. Pratyush - https://github.com/pratyush4932                                     /
-/ 2. Ahmii - https://github.com/Ahmii-kun                                           /               
-/ 3. Kai - https://github.com/Kai0071                                               /                    
-/ 4. Devime - https://github.com/Devime69                                           /
-/ 5. Jay JayOps - https://github.com/jayjay-ops                                     /
-/                                                                                   /
-/ ----------------------------------------------------------------------------------/
-/                                                                                   /
-/      With all of our hard work and defication you can enjoy this awesome bot!     /  
-/                                                                                   / 
-/----------------------------------------------------------------------------------*/
+/ Author and Main Developer: N.C.T Association                                      /
+/ Github: https://github.com/Push-d/N.C.T Association                               /
+/ Powered By: Team N.C.T                                                            /
+/*-----------------------------------------------------------------------------------*/
 
 
 require("dotenv").config();
@@ -44,8 +31,8 @@ global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey =
   process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
-global.packname = process.env.PACKNAME || `Atlas MD`;
-global.author = process.env.AUTHOR || "by: Team Atlas";
+global.packname = process.env.PACKNAME || `N.C.T`;
+global.author = process.env.AUTHOR || "by: Team N.C.T";
 global.port = process.env.PORT || "8000";
 
 module.exports = {
@@ -56,18 +43,18 @@ module.exports = {
 
 global.mess = {
   jobdone: "Job done...",
-  useradmin: "Sorry, only *Group Admins* can use this command *Baka*!",
+  useradmin: "Sorry, only *Group Admins* can use this command!",
   botadmin:
     "Sorry, i cant execute this command without being an *Admin* of this group.",
-  botowner: "Only my *Owner* can use this command, Baka!",
-  grouponly: "This command is only made for *Groups*, Baka!",
-  privateonly: "This command is only made for *Private Chat*, Baka!",
+  botowner: "Only my *Owner* can use this command you fool!",
+  grouponly: "This command is only made for *Groups*!",
+  privateonly: "This command is only made for *Private Chat*!",
   botonly: "Only the *Bot itself* can use this command!",
-  waiting: "Chotto Matte...",
-  nolink: "Please provide me *link*, Baka!",
+  waiting: "Please wait....",
+  nolink: "Please provide me *link*!",
   error: "An error occurd!",
-  banned: `You are *Banned* fron using commands!  \n\nType *${prefa}owner* or *${prefa}support* to submit a request to unban yourself !`,
+  banned: `You are *Banned* fron using commands!  \n\nType *${prefa}owner* to submit a request to unban yourself !`,
   bangc: "This Group is *Banned* from using Commands!",
-  nonsfw: "Dont be a pervert Baka! This is not a NSFW enabled group!",
+  nonsfw: "Dont be a pervert Broh! This is not a NSFW enabled group!",
 };
 
